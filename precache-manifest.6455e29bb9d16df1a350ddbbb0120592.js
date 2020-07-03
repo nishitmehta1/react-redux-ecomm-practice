224b50f6091c7ed3d4ba67ef2cee222a7650c0fd
Nishit Mehta <nishit.mehta1@gmail.com>
2020-07-03 15:31:37 -0700

    Updates

---
+++ precache-manifest.6455e29bb9d16df1a350ddbbb0120592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ed00b78c1368a9ce4c8b194d13936b",
+    "url": "/react-redux-ecomm-practice/index.html"
+  },
+  {
+    "revision": "494897b78249f3f08c9d",
+    "url": "/react-redux-ecomm-practice/static/css/main.a3235377.chunk.css"
+  },
+  {
+    "revision": "7f975eb448a2b0f24409",
+    "url": "/react-redux-ecomm-practice/static/js/2.75216520.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-redux-ecomm-practice/static/js/2.75216520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494897b78249f3f08c9d",
+    "url": "/react-redux-ecomm-practice/static/js/main.0b598476.chunk.js"
+  },
+  {
+    "revision": "14bc8da0caaad854b917",
+    "url": "/react-redux-ecomm-practice/static/js/runtime-main.74d93bad.js"
+  }
+]);
